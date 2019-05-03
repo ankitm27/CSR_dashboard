831cfce589b7331bc27bcfccf4ae1b4923c486e1
mukesh4601 <bhardwaj.mukesh91@gmail.com>
2019-05-03 18:00:39 +0530

    SignUp API

--- src/Components/Forms/Signup/Signup.jsx
+++ src/Components/Forms/Signup/Signup.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './Signup.css';
 
+import { connect } from "react-redux";
+import actions from "../../../Store/Actions/Index";
+import { withRouter } from "react-router";
+
 import { Button, Form, InputGroup } from 'react-bootstrap';
 
 class Signup extends Component {
@@ -12,6 +16,31 @@ class Signup extends Component {
         };
     }
 
+    state = {
+        email: "",
+        password: "",
+        firstname: "",
+        lastname: "",
+        mobile: "",
+        password: '',
+        organizationType: "",
+        confirmPassword: ""
+    }
+
+
+    onSubmit = (evt) => {
+        evt.preventDefault();
+        console.dir(this.state.email, this.state.password)
+        if (this.state.firstname === "" || this.state.password === "" || this.state.confirmPassword === "" || this.state.lastname === "" || this.state.mobile === "" || this.state.organizationType === "") {
+            document.getElementById("emailerror").innerHTML = "Enter All Values";
+        }
+        else {
+            document.getElementById("emailerror").innerHTML = "";
+            this.props.registerUser({ email: this.state.email, password: this.state.password, confirmPassword: this.state.confirmPassword, firstname: this.state.firstname, lastname: this.state.lastname, mobile: this.state.mobile, organizationType: this.state.organizationType })
+            return;
+        }
+    }
+
     render() {
         return (
             <div className="col-md-11 signupform">
@@ -21,33 +50,43 @@ class Signup extends Component {
                         <p>Filling your detail to get Access</p>
                     </div>
                     <div className="col-md-12 text-left mbform">
-                        <Form>
+                        <Form onSubmit={this.onSubmit}>
                             <div className="row">
 
                                 <div className="col-md-6 ">
                                     <Form.Group controlId="formBasicEmail">
-                                        <Form.Control type="text" placeholder="First Name" />
+                                        <Form.Control type="text" placeholder="First Name" value={this.state.firstname} onChange={(evt) => {
+                                            this.setState({ firstname: evt.target.value })
+                                        }} />
                                     </Form.Group>
                                 </div>
                                 <div className="col-md-6 ">
                                     <Form.Group controlId="formBasicEmail">
-                                        <Form.Control type="text" placeholder="Last Name" />
+                                        <Form.Control type="text" placeholder="Last Name" value={this.state.lastname} onChange={(evt) => {
+                                            this.setState({ lastname: evt.target.value })
+                                        }} />
                                     </Form.Group>
                                 </div>
                                 <div className="col-md-6 ">
                                     <Form.Group controlId="formBasicEmail">
-                                        <Form.Control type="email" placeholder="Email" />
+                                        <Form.Control type="email" placeholder="Email" value={this.state.email} onChange={(evt) => {
+                                            this.setState({ email: evt.target.value })
+                                        }} />
                                     </Form.Group>
                                 </div>
                                 <div className="col-md-6 ">
                                     <Form.Group controlId="formBasicEmail">
-                                        <Form.Control type="number" placeholder="Mobile Number" />
+                                        <Form.Control type="number" placeholder="Mobile Number" value={this.state.mobile} onChange={(evt) => {
+                                            this.setState({ mobile: evt.target.value })
+                                        }} />
                                     </Form.Group>
                                 </div>
 
                                 <div className="col-md-12 ">
                                     <Form.Group controlId="formBasicEmail">
-                                        <Form.Control as="select">
+                                        <Form.Control as="select" value={this.state.organizationType} onChange={(evt) => {
+                                            this.setState({ organizationType: evt.target.value })
+                                        }}  >
                                             <option>Organisation Type</option>
                                             <option>Goverment</option>
                                             <option>NGO</option>
@@ -63,7 +102,9 @@ class Signup extends Component {
                                             type="password"
                                             placeholder="Password"
                                             aria-describedby="inputGroupPrepend"
-                                            required />
+                                            required value={this.state.password} onChange={(evt) => {
+                                                this.setState({ password: evt.target.value })
+                                            }} />
                                         <InputGroup.Prepend>
                                             <InputGroup.Text id="inputGroupPrepend"><i className="fa fa-eye" aria-hidden="true"></i></InputGroup.Text>
                                         </InputGroup.Prepend>
@@ -75,7 +116,9 @@ class Signup extends Component {
                                             type="password"
                                             placeholder="Confirm Password"
                                             aria-describedby="inputGroupPrepend"
-                                            required />
+                                            required value={this.state.confirmPassword} onChange={(evt) => {
+                                                this.setState({ confirmPassword: evt.target.value })
+                                            }} />
                                         <InputGroup.Prepend>
                                             <InputGroup.Text id="inputGroupPrepend"><i className="fa fa-eye" aria-hidden="true"></i></InputGroup.Text>
                                         </InputGroup.Prepend>
@@ -89,6 +132,10 @@ class Signup extends Component {
                                         Sign Up Now
                                     </Button>
                                 </div>
+
+                                <div className="col-md-6 text-right mb50">
+                                    <p id="emailerror" className="text-danger"></p>
+                                </div>
                             </div>
                         </Form>
                     </div>
@@ -99,4 +146,11 @@ class Signup extends Component {
     }
 }
 
-export default Signup;
+const mapStateToProps = state => ({
+    auth: state.auth,
+})
+const mapDispatchToProps = dispatch => ({
+    registerUser: (v) => dispatch(actions.registerUser(v)),
+})
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Signup));
