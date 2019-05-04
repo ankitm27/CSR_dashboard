8cf50fd5debee88b0a44c5e8c4d77f8bede28ff5
mukesh4601 <bhardwaj.mukesh91@gmail.com>
2019-05-04 10:20:34 +0530

    REDUCERS FOR PROJECTS

--- src/Store/Reducers/index.js
+++ src/Store/Reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import AuthReducer from "./AuthReducer";
+import ProjectReducers from "./ProjectsReducers";
 
 
 export default combineReducers({
     auth: AuthReducer,
+    Projects: ProjectReducers
 })
