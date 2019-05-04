d6eb3c473b3451b8a0e9cf58a14483060267f4e0
mukesh4601 <bhardwaj.mukesh91@gmail.com>
2019-05-04 11:36:50 +0530

    Card UI

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -6,7 +6,16 @@ class Projects extends Component {
         super();
         this.state = {
             Title: 'Projects',
-            Button: 'Create New Project'
+            Button: 'Create New Project',
+            ProjectName: 'Project 1',
+            Status: 'Open',
+            LastUpdatedAt: '5 hours ago',
+            Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+            Goal: 'Clean water and sustainability ',
+            Average: '70%',
+            Result: 'Good',
+            DaysLeft: '8 Days left',
+            Funding: '$1200'
         };
     }
     render() {
@@ -20,8 +29,57 @@ class Projects extends Component {
                 </div>
 
                 <div className="col-md-4 projectlist">
-                    <div className="row projectcard"></div>
+                    <div className="row projectcard">
+                        <div className="col-md-6 left">
+                            <h1>{this.state.ProjectName}</h1>
+                            <p>{this.state.LastUpdatedAt}</p>
+                        </div>
+                        <div className="col-md-6 right text-right">
+                            <button type="button" class="btn btn-status"> <i className="fa  fa-circle"></i> {this.state.Status}</button>
+                        </div>
+                        <div className="col-md-12  text-left description">
+                            <p>{this.state.Description}</p>
+                        </div>
+                        <div className="col-md-12  text-left goal">
+                            <p>Goal : <span>{this.state.Goal}</span></p>
+                        </div>
+                        <div className="col-md-11  text-left goalachived">
+                            <p>Goal Achieved <span className="text-right"> {this.state.Average}</span></p>
+                        </div>
+                        <hr></hr>
+
+                        <div className="col-md-12  text-left result">
+                            <p>Result <span className="text-right"> {this.state.Result}</span></p>
+                        </div>
+                        <div className="col-md-12">
+                            <div class="progress">
+                                <div class="progress-bar bg-success" role="progressbar" style={{ width: '15%' }} aria-valuenow="15" aria-valuemin="0" aria-valuemax="100"></div>
+                                <div class="progress-bar bg-danger" role="progressbar" style={{ width: '50%' }} aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
+                                <div class="progress-bar bg-warning" role="progressbar" style={{ width: '20%' }} aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                        <hr></hr>
+
+                        <div className="col-md-6">
+                            <button type="button" class="btn btn-daysleft"> <i className="fa fa-clock-o"></i> {this.state.DaysLeft}</button>
+                        </div>
+                        <div className="col-md-6  text-left funding">
+                            <p>Funding: {this.state.Funding}</p>
+                        </div>
+                    </div>
                 </div>
+
+
+
+
+
+
+
+
+
+
+
+
 
             </div>
         );
